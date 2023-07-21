d72aac2fe575a16a8cdf04fd82fbbdad336fd3e7
rickon43 <113466430+rickon43@users.noreply.github.com>
2023-07-21 12:17:53 +0530

    json files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+
+  extends: ['base'],
+
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '.eslintrc.js',
+    'commitlint.config.js',
+    'list-duplicates.js',
+    'vitest.config.ts',
+    'vite.config.ts',
+  ],
+};
